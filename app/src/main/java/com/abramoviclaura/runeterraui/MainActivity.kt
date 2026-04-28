577355927e0bac9eebf35145bff18caebd1e8afb
laura-abramovic <abramovic.laura@gmail.com>
2026-04-28 17:15:14 +0200

    RUI-5: Add splash screen

--- app/src/main/java/com/abramoviclaura/runeterraui/MainActivity.kt
+++ app/src/main/java/com/abramoviclaura/runeterraui/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -38,6 +39,7 @@ import com.abramoviclaura.runeterraui.ui.theme.RuneterraUITheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
